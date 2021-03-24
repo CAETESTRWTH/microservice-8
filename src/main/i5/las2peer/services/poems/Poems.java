f0f51ca03c0948f4e05c72ac5118079ec722dfe4
erdzan12 <jungerjunge2@gmail.com>
2021-03-24 01:20:30 +0000

    Http Response[response] edited by Erdzan Rastoder

--- src/main/i5/las2peer/services/poems/Poems.java
+++ src/main/i5/las2peer/services/poems/Poems.java
@@ -125,6 +125,7 @@ JSONObject temp = new JSONObject();
 JSONObject temp1 = new JSONObject();
             temp1.put("text", "Sein Blick ist vom Vorübergehn der Stäbe so müd geworden, dass er nichts mehr hält. Ihm ist, als ob es tausend Stäbe gäbe und hinter tausend Stäben keine Welt. Der weiche Gang geschmeidig starker Schritte, der sich im allerkleinsten Kreise dreht, ist wie ein Tanz von Kraft um eine Mitte, in der betäubt ein großer Wille steht. Nur manchmal schiebt der Vorhang der Pupille sich lautlos auf -. Dann geht ein Bild hinein, geht durch der Glieder angespannte Stille - und hört im Herzen auf zu sein.");
   a.add(temp1);
+        return Response.ok(a.toJSONString()).build();
 
     }
     return null;
